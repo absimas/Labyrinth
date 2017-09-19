18c3efe6b9dbbb2f26ae5f90e62aa5a746e23a94
Simas <ab.simas@gmail.com>
2017-09-19 20:35:00 +0300

    Add methods parse and write input.

--- src/Labyrinth.kt
+++ src/Labyrinth.kt
@@ -1,9 +1,15 @@
+import java.io.File
+import java.util.regex.Pattern
+
 /**
  * Created by Simas on 2017 Sep 19.
  */
 class Labyrinth {
 
   companion object {
+    inline fun <reified INNER> array2d(sizeOuter: Int, sizeInner: Int, noinline innerInit: (Int) -> INNER): Array<Array<INNER>>
+        = Array(sizeOuter) { Array(sizeInner, innerInit) }
+
     @JvmStatic
     fun main(args: Array<String>) {
       Labyrinth()
@@ -11,10 +17,76 @@ class Labyrinth {
   }
 
   init {
-    val n: Int = readNum("Enter n:", "'%s' must be in bounds of [1..7]") { n: Int ->
-      n in 1..7
-    }!!
-    println("Read $n!")
+    val array = parseInput()
+    printArray2d(array)
+
+//    val n: Int = readNum("Enter n:", "'%s' must be in bounds of [1..7]") { n: Int ->
+//      n in 1..7
+//    }!!
+//    println("Read $n!")
+  }
+
+  private fun parseInput(): Array<Array<Int>> {
+    // Open input file
+    val input = File("input")
+    if (!input.exists()) {
+      throw IllegalStateException("Input file must exist!")
+    }
+
+    val rows = input.readLines()
+    val rowCount = rows.size
+    if (rowCount == 0) {
+      throw IllegalStateException("Input file must contain rows separated by new lines!")
+    }
+    val colCount = rows[0].split(Pattern.compile("\\s")).size
+    if (colCount == 0) {
+      throw IllegalStateException("Input file must contain cols separated by white spaces!")
+    }
+
+    val array = array2d(rowCount, colCount) { 0 }
+
+    rows.forEachIndexed { rowIndex, line ->
+      // Get cols as strings
+      val colStrings = line.split(Pattern.compile("\\s"))
+      if (colStrings.size != colCount) {
+        throw IllegalStateException("Input file rows must contain an equal amount of cols!")
+      }
+
+      // Parse cols into ints
+      val colInts: Array<Int> = colStrings.map {
+        val int = it.toIntOrNull()
+        if (int == null || int !in 0..1 ) {
+          throw IllegalStateException("Unexpected cell character found: '$it'! Must be either 0 or 1.")
+        }
+        int!!
+      }.toTypedArray()
+
+      // Input rows are numbered in a descending fashion so write to the last one first
+      array[rowCount - rowIndex - 1] = colInts
+    }
+
+    return array
+  }
+
+  private fun printArray2d(array: Array<Array<Int>>) {
+    if (array.isEmpty()) return
+
+    println("Y, V ^")
+    for (i in array.size-1 downTo 0) {
+      print(String.format("  %2d | ", i+1))
+      for (j in 0 until array.size) {
+        print(String.format("%2d ", array[i][j]))
+      }
+      println()
+    }
+    print("     ")
+    print("-".repeat(3 * array[0].size + 1))
+    println("> X, U")
+    print("       ")
+    for (i in 0 until array[0].size) {
+      print(String.format("%2d ", i+1))
+    }
+    println()
   }
 
   /**
